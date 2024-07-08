94db74a979954669e1d4de31802f4423c086f36b
chitose1020 <chitose1020@outlook.jp>
2024-07-08 22:34:14 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+window.onload = () => {
 //ドロップダウンの作成
 const sel1 = new UI.dropdown(document.getElementById("sel1"));
 sel1.setOnChange((value) => {console.log(value);});
@@ -5,3 +6,4 @@ const sel2 = new UI.dropdown(document.getElementById("sel2"));
 //スライダーの作成
 const slider1 = new slider(document.getElementById("slider1"));
 slider1.setstates(150,50,0);
+}
