4b13928a2fe64a70eb575cf65f3eea728008a8e3
chitose1020 <chitose1020@outlook.jp>
2024-07-08 22:27:29 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+//ドロップダウンの作成
+const sel1 = new UI.dropdown(document.getElementById("sel1"));
+sel1.setOnChange((value) => {console.log(value);});
+const sel2 = new UI.dropdown(document.getElementById("sel2"));
+//スライダーの作成
+const slider1 = new slider(document.getElementById("slider1"));
+slider1.setstates(150,50,0);
