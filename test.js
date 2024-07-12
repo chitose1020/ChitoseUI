f869632bb51ed93a448694eb84e2d764724fb9fb
chitose1020 <chitose1020@outlook.jp>
2024-07-13 08:13:39 +0900

    Add slider style

--- test.js
+++ test.js
@@ -3,8 +3,10 @@ window.onload = () => {
 const sel1 = new UI.dropdown(document.getElementById("sel1"));
 sel1.setOnChange((value) => {console.log(value);});
 const sel2 = new UI.dropdown(document.getElementById("sel2"));
+sel2.setMaxHeight(50);
 //スライダーの作成
 const slider1 = new slider(document.getElementById("slider1"));
-slider1.setstates(150,50,0);
-
+slider1.setRange(50,0);
+slider1.setWidth(300);
+slider1.setColors("black","blue");
 }
