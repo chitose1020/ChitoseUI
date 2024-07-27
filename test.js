55aa197b6a7719a0191d2b6f9da573d1499fa6fa
chitose1020 <chitose1020@outlook.jp>
2024-07-27 17:15:03 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 window.onload = () => {
 //ドロップダウンの作成
-const sel1 = new UI.dropdown(document.getElementById("sel1"));
+const sel1 = UI.dropdown(document.getElementById("sel1"));
 sel1.setOnChange((value) => {console.log(value);});
-const sel2 = new UI.dropdown(document.getElementById("sel2"));
+const sel2 = UI.dropdown(document.getElementById("sel2"));
 sel2.setMaxHeight(50);
 //スライダーの作成
-const slider1 = new slider(document.getElementById("slider1"));
+const slider1 = UI.slider(document.getElementById("slider1"));
 slider1.setRange(50,0);
 slider1.setWidth(300);
 slider1.setColors("black","blue");
-const loading1 = new loading(document.getElementById("loading1"));
+const loading1 = UI.loading(document.getElementById("loading1"));
 }
